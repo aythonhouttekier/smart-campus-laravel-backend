2a09ba02f0efec017e02bbc8519376ad274eb0f0
Steve Trap <trap.steve@hotmail.com>
2018-03-18 09:43:08 +0100

    Added api get(id, index) and post

---
+++ app/Http/Controllers/LocationsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Resources\locations as LocationsResource;
+use App\locations;
+
+class LocationsController extends Controller
+{
+    public function index()
+    {
+        return locations::all();
+    }
+ 
+    public function show($id)
+    {
+        return locations::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        if ($request->has(['classroom'])) {
+            return locations::create($request->all());
+        } else {
+            echo "Wrong format to store";
+        }
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -15,12 +15,16 @@ Use App\measurements;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 Route::get('measurements', 'MeasurementsController@index');
 Route::get('measurements/{id}', 'MeasurementsController@show');
 Route::post('measurements', 'MeasurementsController@store');
+
+Route::get('locations', 'LocationsController@index');
+Route::get('locations/{id}', 'LocationsController@show');
+Route::post('locations', 'LocationsController@store');
 
 Route::post('listener', 'TTNDataController@store');
