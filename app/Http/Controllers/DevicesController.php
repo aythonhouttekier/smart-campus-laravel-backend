9e8e92a089779e91a7d17dd6f0fe66a487331f5a
Steve Trap <trap.steve@hotmail.com>
2018-03-19 10:16:59 +0100

    Added basic controller code for sensors and devices

--- app/Http/Controllers/DevicesController.php
+++ app/Http/Controllers/DevicesController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class DevicesController extends Controller
 {
-    //
+    public function index()
+    {
+        return devices::all();
+    }
+ 
+    public function show($id)
+    {
+        return devices::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        if ($request->has(['device_name'])) {
+            return devices::create($request->all());
+        } else {
+            echo "Wrong format to store";
+        }
+        
+    }
 }
--- app/Http/Controllers/SensorsController.php
+++ app/Http/Controllers/SensorsController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class SensorsController extends Controller
 {
-    //
+    public function index()
+    {
+        return sensors::all();
+    }
+ 
+    public function show($id)
+    {
+        return sensors::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        if ($request->has(['sensor_name', 'measurement_unit'])) {
+            return sensors::create($request->all());
+        } else {
+            echo "Wrong format to store";
+        }
+        
+    }
 }
