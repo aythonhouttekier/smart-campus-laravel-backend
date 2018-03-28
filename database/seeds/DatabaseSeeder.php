f5ca4c2653a36dbca0822294936f45468ce27501
Steve Trap <trap.steve@hotmail.com>
2018-03-28 19:17:23 +0200

    added seeder with dummy info

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\locations;
+use App\devices;
+use App\sensors;
 use App\measurements;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,52 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         Eloquent::unguard();
+
+        $this->call('SmartCampusDummySeeder');
+        $this->command->info('Smart campus dummy seeds finished.');
     }
+}
+
+class SmartCampusDummySeeder extends Seeder {
+
+    public function run() {
+
+        DB::table('locations')->delete();
+        DB::table('devices')->delete();
+        DB::table('sensors')->delete();
+        DB::table('measurements')->delete();
+
+
+        $roomEmbedded = locations::create(array(
+            'name'         => 'Embedded systems',
+            'roomnumber'   => '2.65',
+            'description'  => 'Our hackerspace lab, sometimes there is some education also'
+        ));
+
+        $this->command->info('Classroom created!');
+
+        $device = devices::create(array(
+            'name'        => 'dummy_device',
+            'dev-eui'     => '00E4F052209EE8A5',
+            'location_id' => $roomEmbedded->id
+        ));
+
+        $this->command->info('We have put a device in a classroom');
+
+        $dummySensor = sensors::create(array(
+            'name'             => 'Dummy_sensor',
+            'measurement_unit' => 'Dummy_unit',
+            'device_id' => $device->id
+        ));
+
+        $this->command->info('Dummy sensor created, you can add dummy measurments with the api');
+
+        measurements::create(array(
+            'value' => 20,
+            'sensor_id' => $dummySensor->id
+        ));
+
+        $this->command->info('Added one value');
+    }
+
 }
