ea97182a03318f25567de021e56c0ad20d8a56ed
Steve Trap <trap.steve@hotmail.com>
2018-04-08 19:41:27 +0200

    added var_dump for user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,6 +46,7 @@ class UserController extends Controller
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
+        var_dump($user);
         $success['token'] =  $user->createToken('MyApp')->accessToken;
         var_dump($success['token']);
         $success['name'] =  $user->name;
