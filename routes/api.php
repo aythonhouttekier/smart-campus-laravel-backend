12bdc2f56e11acd9a1b51167df519edabc6f4cb0
Steve Trap <trap.steve@hotmail.com>
2018-04-14 11:54:42 +0200

    made some routes public for testing at the frontend

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,11 @@ use Illuminate\Http\Request;
 Route::get('index', 'UserController@index');
 Route::post('register', 'UserController@register');
 
+// This routes are for test purpose at the frontend
+Route::post('locations', 'LocationsController@store');
+Route::post('sensors', 'SensorsController@store');
+Route::post('devices', 'DevicesController@store');
+
 //Login for posting
 Route::post('login', 'UserController@login');
 Route::group(['middleware' => 'auth:api'], function(){
